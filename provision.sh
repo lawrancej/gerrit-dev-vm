582f8d8fb308bae93c3406ee6a48a2da6e59e77c
Joey Lawrance <lawrancej@wit.edu>
2014-06-10 23:45:38 -0400

    Finally got it working in a shared folder.

--- provision.sh
+++ provision.sh
@@ -5,11 +5,20 @@
 sudo apt-get update
 sudo apt-get upgrade
 
+# Download build tools
 sudo apt-get install -y zip curl
 sudo apt-get install -y build-essential automake autoconf
-sudo apt-get install -y openjdk-7-jre-headless openjdk-7-jdk ant
 sudo apt-get install -y git
 
+# Download Java, Ant
+sudo apt-get install -y python-software-properties
+sudo sh -c 'yes | add-apt-repository ppa:webupd8team/java'
+sudo apt-get update
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
+sudo apt-get install -y ant
+
+# Get watchman installed
 git clone https://github.com/facebook/watchman.git
 cd watchman
 ./autogen.sh
@@ -18,11 +27,17 @@ make
 sudo make install
 cd ..
 
+# Get Buck
 echo "Please wait, this will take a while..."
 git clone https://gerrit.googlesource.com/buck
 cd buck
 sudo sh -c 'echo "PATH=$(pwd)/bin:$PATH" >> /etc/profile'
 cd ..
+
+cat > .buckrc <<EOF
+export BUCK_EXTRA_JAVA_ARGS="-XX:MaxPermSize=512m -Xms8000m -Xmx16000m"
+EOF
+sudo chown vagrant.vagrant .buckrc
 
 sudo chown --recursive vagrant.vagrant watchman
 sudo chown --recursive vagrant.vagrant buck
